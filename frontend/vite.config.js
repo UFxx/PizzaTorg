ca3aa696caebe256e3e30ee8c0b2be2afee69582
UFxx <smileprost@gmail.com>
2024-11-07 23:45:43 +0300

    redeploy try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { host: '172.19.0.3', port: 5173 }
+  server: { port: 5173 }
 });
