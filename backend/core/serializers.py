cd1cb00ee8e5507f208b274db9a1cac593b5ebbc
alexkhub <aleksandrkhubaev04@gmail.com>
2024-11-14 22:11:12 +0300

    fix error

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -136,3 +136,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id',  'first_name', 'last_name', 'email', 'phone', 'address',)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.username = validated_data.get('email', instance.email)
+        instance.phone = validated_data.get('phone', instance.phone)
+        instance.address = validated_data.get('address', instance.address)
+
+        instance.save()
+        return  instance
