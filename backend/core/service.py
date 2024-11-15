fc49545e3518dc8822055a69ed39c146ced350d2
alexkhub <aleksandrkhubaev04@gmail.com>
2024-11-15 21:30:32 +0300

    fix error

--- backend/core/service.py
+++ backend/core/service.py
@@ -1,6 +1,6 @@
 from django.core.mail import send_mail
 
-HOST = 'http://127.0.0.1:8000/admin/core/order/'
+HOST = 'http://31.128.41.110:8000/admin/core/order/'
 
 def send_new_order_email(order_id):
     send_mail(
